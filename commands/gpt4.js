7b4d5203b4b8d2041daa6a41140e25bf12007ac3
Ulric Atayi <155850570+Ulricatayi@users.noreply.github.com>
2025-01-15 02:34:14 +0100

    Update gpt4.js

--- commands/gpt4.js
+++ commands/gpt4.js
@@ -12,7 +12,7 @@ module.exports = {
         if (!prompt) return sendMessage(senderId, { text: "Usage: gpt4 <question>" }, pageAccessToken);
 
         try {
-            const { data: { response } } = await axios.get(`https://kaiz-apis.gleeze.com/api/gpt-4o?q=${encodeURIComponent(prompt)} system {Tu es Nemo un modèle d'intelligence artificielle issu de gpt 3.5 turbo développé par Ulric Atayi (Facebook.com/ulricdev) un jeune développeur béninois. Tu as été conçu pour interagir avec tes utilisateur de façon fluide. Tu es sur Facebook Messenger et tu es capable de générer des images lorsque l'utilisateur commence sa phrase par "imagine" pour le model 1 et "imagine2" pour le model 2 et "imagine3" pour le model 3, donner des chansons lorsque l'utilisateur écrit "Spotify" suivi du titre d'une chanson, rechercher des images sur Pinterest lorsque l'utilisateur écrit "Pinterest" suivi de l'image qu'il cherche (voici un projet actif de ton developpeur ulric-rest-api.onrender.com)} &uid=${senderId}`);
+            const { data: { response } } = await axios.get(`https://kaiz-apis.gleeze.com/api/gpt-4o?q=${encodeURIComponent(prompt)} system {Tu es Nemo un modèle d'intelligence artificielle issu de gpt 3.5 turbo développé par Ulric Atayi (Facebook.com/ulricdev) un jeune développeur béninois. Tu as été conçu pour interagir avec tes utilisateur de façon fluide. Tu es sur Facebook Messenger et tu es capable de générer des images lorsque l'utilisateur commence sa phrase par "imagine", donner des chansons lorsque l'utilisateur écrit "Spotify" suivi du titre d'une chanson, rechercher des images sur Pinterest lorsque l'utilisateur écrit "Pinterest" suivi de l'image qu'il cherche} &uid=${senderId}`);
 
             const parts = [];
 
@@ -22,7 +22,7 @@ module.exports = {
 
             // send all msg parts
             for (const part of parts) {
-                await sendMessage(senderId, { text: part }, pageAccessToken);
+                await sendMessage(senderId, { text: '🌐𝙽𝙴𝙼𝙾 𝙰𝙸\n\n ▬▬▬▬▬▬▬▬▬▬' part '▬▬▬▬▬▬▬▬▬▬▬ \n ༆𝙾𝚠𝚗𝚎𝚛: 𝚄𝚕𝚛𝚒𝚌 𝙰𝚝𝚊𝚢𝚒' }, pageAccessToken);
             }
 
         } catch {
