833711ba7e2b6eec7831e45b5332731a4eb83fb5
Ulric Atayi <155850570+Ulricatayi@users.noreply.github.com>
2025-03-02 01:19:48 +0100

    Update gpt4.js

--- commands/gpt4.js
+++ commands/gpt4.js
@@ -21,9 +21,9 @@ module.exports = {
 
       // send all msg parts
       for (const part of parts) {
-        await sendMessage(senderId, { text: `🤖𝗡𝗘𝗠𝗢 𝗔𝗜 `n\ n\` + part }, pageAccessToken);
+        await sendMessage(senderId, { text: `🤖𝗡𝗘𝗠𝗢 𝗔𝗜 n\ n\` + part }, pageAccessToken);
       }
-      sendMessage(senderId, { text: `Partagez ce lien avec vos amis : `n\ n\ `🌐facebook.com/nemogpt` }, pageAccessToken);
+      sendMessage(senderId, { text: `Partagez ce lien avec vos amis :🌐facebook.com/nemogpt` }, pageAccessToken);
     } catch {
       sendMessage(senderId, { text: 'Veuillez réessayer s\'il vous plait, vous êtes très nombreux et mon serveur est un peu surchargé. :(' }, pageAccessToken);
     }
